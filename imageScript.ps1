54c321a66fb9178d88e2799d4fe423fd226db77b
TripleR <54783381+R-Triple@users.noreply.github.com>
2019-09-03 18:53:31 -0700

    Update imageScript.ps1

--- imageScript.ps1
+++ imageScript.ps1
@@ -1,6 +1,12 @@
+## Script to run after imaging as it configures and installs necessary programs missed by SCCM ##
+## USB with essential files must be plugged in ##
+
 Set-executionpolicy -executionPolicy Unrestricted LocalMachine
 Get-Process vpnui | stop-process -force
 Copy-Item -Force -Recurse -Path D:\Cisco\ -Destination C:\ProgramData\
-exec /i D:\asdfasdf.msi /quiet /qf /norestart /log C:\Temp\
+exec /i D:\*.msi /quiet /qf /norestart /log C:\Temp\
+
+## Adds Computer to Domain ##
 add-computer -domainname global.com -Credential global\david.long
+
 run \\lagprint\CanonPrint-LAGPRINT
