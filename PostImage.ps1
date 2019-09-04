3d1ef7b10e4af03eddad9961776b398f5976a2a3
TripleR <54783381+R-Triple@users.noreply.github.com>
2019-09-03 18:47:55 -0700

    Update PostImage.ps1

--- PostImage.ps1
+++ PostImage.ps1
@@ -1,6 +1,8 @@
+## Script to run after user Logs Into Newly Imaged Machine ##
+
 set-executionpolicy -ExecutionPolicy "ByPass"
-net use T: \\lacentral\Applications /persistent:no /savecred
-net use S: \\lacentral\Planning /persistent:no /savecred
+net use T: \\lacentral\Applications /persistent:yes /savecred
+net use S: \\lacentral\Planning /persistent:yes /savecred
 start-process \\lagprint\CanonPrint-LAGPRINT
 netsh wlan connect ssid=OMCGroup
 D:\Scripts\SetDefaultPrinter.ps1 -PrinterName "CanonPrint-LAGPRINT"
